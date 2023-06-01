ef445ff718f8cf4c028d8ffb19eb9fa2dd786e29
Partial <133568898+Partial206@users.noreply.github.com>
2023-06-01 16:32:29 +0300

    Update atm.java

--- atm.java
+++ atm.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class atm {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int money = 0;
+        double money = 0;
         int wh = 0;
+        double total = 0;
+        double[] board = {1000.1,2000.2,3000.3,4000.4,5000.5,money};
+        double max = board[0];
         while(wh < 50){
-         System.out.println(" 1 : Withdraw / 2 : Deposit / 3 : Money");
+         System.out.println(" 1 : Withdraw / 2 : Deposit / 3 : Money / 4: Moneyboard / 5: Total Money");
          int process = scan.nextInt();
          wh++;
          switch(process){
@@ -20,16 +23,34 @@ public class atm {
                 System.out.println("You cant withdraw money");;
              }
              break;
+
             case 2:
              System.out.println("How many?");
              int deposit = scan.nextInt();
-             money += deposit;
+             deposit +=money;
              break;
+
             case 3:
              System.out.println("Your money:" + money);
              break;
-        }
-        
+
+            case 4:
+            for(double number:board){
+               if(max < number){
+                  max = number;
+               }
+            }
+            System.out.println("Most money in the Bank :" + max);
+            break;
+
+            case 5:
+             for(double numberr:board){
+               total = numberr + total;
+             }
+            System.out.println("Total money in the Bank :" + total);
+            break; 
+        }        
     }
   }
 }
+
